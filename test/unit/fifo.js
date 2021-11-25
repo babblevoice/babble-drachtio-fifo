312420f6176bdd358d046274a81a0b37b1e28177
nick <nick@babblevoice.com>
2021-11-25 11:40:04 +0000

    2 more tests

--- test/unit/fifo.js
+++ test/unit/fifo.js
@@ -4,6 +4,8 @@ const expect = require( "chai" ).expect
 const events = require( "events" )
 
 const fifo = require( "../../lib/fifo.js" )
+const registrar = require( "../mock/registrar.js" )
+const srf = require( "../mock/srf.js" )
 
 
 describe( "fifo.js", function() {
@@ -13,7 +15,7 @@ describe( "fifo.js", function() {
     expect( f ).to.have.property( "_fifos" ).that.is.a( "Array" )
   } )
 
-  it( `Queue a ringall`, async function() {
+  it( `Queue a ringall - no agents`, async function() {
     let f = fifo.create()
 
     /* mocks */
@@ -37,5 +39,74 @@ describe( "fifo.js", function() {
 
     expect( f._fifos[ 4 ] ).to.be.a( "array" ).to.have.lengthOf( 0 )
     expect( f._callcount ).to.equal( 0 )
+  } )
+
+
+  it( `Queue a ringall - 2 agents`, async function() {
+    /*
+    Add agents but don't supply a registrar so it will fail - we
+    should still queue and clean up well on hangup. This should finish
+    cleanly without leaving unresolved promises/timers.
+    */
+    let f = fifo.create()
+
+    f.agents( {
+      "agents": [ "1000@dummy.com", "1001@dummy.com" ]
+    } )
+
+    /* mocks */
+    let call = {
+      "uuid": "1",
+      "_em": new events.EventEmitter(),
+      "on": ( e, cb ) => call._em.on( e, cb ),
+      "vars": {}
+    }
+
+    f.queue( { call } )
+
+    call._em.emit( "call.hangup", call )
+
+  } )
+
+  it( `Queue a ringall - 2 agents and registrar then abandon`, async function() {
+    /*
+    Add agents but don't supply a registrar so it will fail - we
+    should still queue and clean up well on hangup. This should finish
+    cleanly without leaving unresolved promises/timers.
+    */
+    let options = {
+      "registrar": registrar.create(),
+      "srf": srf.create(),
+      "uactimeout": 10000
+    }
+
+    let f = fifo.create( options )
+
+    options.registrar.addmockcontactinfo( "1000@dummy.com", { "contacts": [ "sip:1@d.c" ] } )
+    options.registrar.addmockcontactinfo( "1001@dummy.com", { "contacts": [ "sip:1@e.c" ] } )
+
+    f.agents( {
+      "agents": [ "1000@dummy.com", "1001@dummy.com" ]
+    } )
+
+    /* mocks */
+    let call = {
+      "uuid": "1",
+      "_em": new events.EventEmitter(),
+      "on": ( e, cb ) => call._em.on( e, cb ),
+      "vars": {}
+    }
+
+    let waiting = f.queue( { call } )
+
+    call._em.emit( "call.hangup", call )
+
+    let reason
+    await waiting
+      .catch( ( e ) => {
+        reason = e
+      } )
+
+    expect( reason ).to.equal( "abandoned" )
   } )
 } )
