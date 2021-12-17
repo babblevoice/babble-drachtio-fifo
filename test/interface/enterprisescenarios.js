06fa2458698b9fdc8e2feb6cca7ecc46d2e6c9b6
nick <nick@babblevoice.com>
2021-12-17 12:38:23 +0000

    next test and bug fix

--- test/interface/enterprisescenarios.js
+++ test/interface/enterprisescenarios.js
@@ -223,4 +223,150 @@ describe( "interface enterprisescenarios.js", function() {
     expect( mockagentcall.agenturicalls.filter( ( v ) => v === "1001@dummy.com" ).length ).to.be.within( 48, 52 )
 
   } )
+
+  it( `main enterprise queue 2 answer 1`, async function() {
+    this.timeout( 2000 )
+    this.slow( 1500 )
+
+    /* create a global fifo object */
+    let globaloptions = {
+      "registrar": registrar.create(),
+      "srf": srf.create(),
+      "uactimeout": 10, /* mS */
+      "agentlag": 10
+    }
+
+    let mainfifo = fifo.create( globaloptions )
+
+    mainfifo.agents( {
+      "domain": "dummy.com",
+      "name": "fifoname",
+      "agents": [ "1000@dummy.com", "1001@dummy.com" ]
+    } )
+
+    /* setup our mock interfaces */
+    globaloptions.registrar.addmockcontactinfo( "1000@dummy.com", { "contacts": [ "sip:1@d.c" ] } )
+    globaloptions.registrar.addmockcontactinfo( "1001@dummy.com", { "contacts": [ "sip:1@e.c" ] } )
+
+    class mockagentcall {
+
+      static lastcreated
+      constructor( uri, parent, callbacks ) {
+        this.uri = uri
+        this._em = new events.EventEmitter()
+        this.parent = parent
+        this._callbacks = callbacks
+
+        mockagentcall.lastcreated = this /* parent is mockinboundcall */
+
+        /* types we can request */
+        this.hangupcodes = {
+          "LOSE_RACE": "LOSE_RACE",
+          "REQUEST_TIMEOUT": "REQUEST_TIMEOUT"
+        }
+      }
+
+      get entity() {
+        return ( async () => {
+          return {
+            "uri": this.uri,
+            "ccc": 0 /* our tests ask this when we have finished */
+          }
+        } )()
+      }
+
+      update() {
+
+      }
+
+      on( ev, cb ) {
+        this._em.on( ev, cb )
+      }
+
+      answer() {
+        this._callbacks.confirm( this )
+      }
+    }
+
+    class mockinboundcall {
+      constructor() {
+        this.uuid = "_" + mockinboundcall.inboundcallcount
+        mockinboundcall.inboundcallcount++
+
+        this._em = new events.EventEmitter()
+        this.vars = {}
+
+      }
+
+      static inboundcallcount = 0
+      static newoutboundcallcount = 0
+
+      on( e, cb ) {
+        this._em.on( e, cb )
+      }
+
+      _killcalls( options, callbacks, agentcall ) {
+        callbacks.early( agentcall )
+
+        setTimeout( () => {
+          /* these are emitted by callmanager - in this order */
+          agentcall._em.emit( "call.destroyed", agentcall )
+          globaloptions.em.emit( "call.destroyed", agentcall )
+          
+        }, options.uactimeout )
+      }
+
+      adopt( childcall ) {
+
+      }
+
+      newuac( options, callbacks ) {
+        mockinboundcall.newoutboundcallcount++
+        this._killcalls( options, callbacks, new mockagentcall( options.entity.uri, this, callbacks ) )
+      }
+    }
+
+    let qitems = [ {
+      "call": new mockinboundcall(),
+      "name": "fifoname",
+      "domain": "dummy.com",
+      "mode": "enterprise",
+      "timeout": 1
+    }, {
+      "call": new mockinboundcall(),
+      "name": "fifoname",
+      "domain": "dummy.com",
+      "mode": "enterprise",
+      "timeout": 1
+    } ]
+
+    /* now back to our inbound call */
+    let waitforfirst = mainfifo.queue( qitems[ 0 ] )
+    let waitforsecond = mainfifo.queue( qitems[ 1 ] )
+
+    expect( mainfifo.getdomain( "dummy.com" ).getfifo( "fifoname" ).size ).to.equal( 2 )
+
+    setTimeout( () => {
+      mockagentcall.lastcreated.answer()
+    }, 200 )
+    
+    let reason = await waitforfirst
+    let reason2 = await waitforsecond
+
+    expect( mainfifo.getdomain( "dummy.com" ).getfifo( "fifoname" ).size ).to.equal( 0 )
+
+    expect( qitems[ 0 ].call.vars.fifo.epochs.leave - qitems[ 0 ].call.vars.fifo.epochs.enter ).to.be.below( 3 ) /* 1S */
+    expect( qitems[ 0 ].call.vars.fifo.state ).to.equal( "confirm" )
+    /*
+    For 2 calls, we have 2 agents ringing. For one call we only have one agent now ringing
+    200mS of 2 agents
+    800mS of 1 agent
+    200 / ( ( 10 + 10 ) / 2 ) = 20
+    800 / ( 10 + 10 ) = 40
+    total = 60 + tolerance
+    */
+    expect( mockinboundcall.newoutboundcallcount ).to.be.within( 57, 63 )
+    expect( reason ).to.equal( "confirm" )
+    expect( reason2 ).to.equal( "timeout" )
+  } )
 } )
