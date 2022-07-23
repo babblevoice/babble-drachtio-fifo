687400643282091c221e06186718850b3ffb0b2c
nick <nick@babblevoice.com>
2022-07-23 17:12:00 +0100

    add test

--- test/interface/scenarios.js
+++ test/interface/scenarios.js
@@ -495,4 +495,167 @@ describe( "interface scenarios.js", function() {
     expect( reason ).to.equal( "confirm" )
     expect( reason2 ).to.equal( "confirm" )
   } )
+
+  it( `main ringall queue 2 abandon 1`, async function() {
+    this.timeout( 2000 )
+    this.slow( 1500 )
+
+    /* create a global fifo object */
+    let globaloptions = {
+      "registrar": registrar.create(),
+      "srf": srf.create(),
+      "uactimeout": 10, /* mS */
+      "agentlag": 10
+    }
+
+    let mainfifo = fifo.create( globaloptions )
+
+    mainfifo.agents( {
+      "domain": "dummy.com",
+      "name": "fifoname",
+      "agents": [ "1000@dummy.com", "1001@dummy.com" ]
+    } )
+
+    /* setup our mock interfaces */
+    globaloptions.registrar.addmockcontactinfo( "1000@dummy.com", { "contacts": [ "sip:1@d.c" ] } )
+    globaloptions.registrar.addmockcontactinfo( "1001@dummy.com", { "contacts": [ "sip:1@e.c" ] } )
+
+    class mockagentcall {
+
+      static lastcreated
+      constructor( uri, parent, callbacks ) {
+        this.uri = uri
+        this._em = new events.EventEmitter()
+        this.parent = parent
+        this._callbacks = callbacks
+
+        mockagentcall.lastcreated = this /* parent is mockinboundcall */
+
+        /* types we can request */
+        this.hangupcodes = {
+          "LOSE_RACE": "LOSE_RACE",
+          "REQUEST_TIMEOUT": "REQUEST_TIMEOUT"
+        }
+      }
+
+      get entity() {
+        return ( async () => {
+          return {
+            "uri": this.uri,
+            "ccc": 0 /* our tests ask this when we have finished */
+          }
+        } )()
+      }
+
+      update() {
+        this.updatecalled = true
+      }
+
+      on( ev, cb ) {
+        this._em.on( ev, cb )
+      }
+
+      answer() {
+        this._callbacks.confirm( this )
+      }
+    }
+
+    class mockinboundcall {
+      constructor() {
+        this.uuid = "_" + mockinboundcall.inboundcallcount
+        mockinboundcall.inboundcallcount++
+
+        this._em = new events.EventEmitter()
+        this.vars = {}
+
+        this.mockfifoupdates = 0
+        this.mockfifopositions = []
+      }
+
+      static inboundcallcount = 0
+      static newoutboundcallcount = 0
+
+      on( e, cb ) {
+        this._em.on( e, cb )
+      }
+
+      off( e, cb ) {
+      }
+
+      emit( ev ) {
+        if( "fifo.update" === ev || "fifo.enter" === ev ) {
+          this.mockfifoupdates++
+          this.mockfifopositions.push( this.vars.fifo.position )
+        }
+      }
+
+      _killcalls( options, callbacks, agentcall ) {
+        callbacks.early( agentcall )
+
+        setTimeout( () => {
+          /* these are emitted by callmanager - in this order */
+          agentcall._em.emit( "call.destroyed", agentcall )
+          globaloptions.em.emit( "call.destroyed", agentcall )
+          
+        }, options.uactimeout )
+      }
+
+      adopt( childcall ) {
+        this.adoptcalled = true
+        this.child = childcall
+      }
+
+      mockhangupafter( timeout ) {
+        setTimeout( () => {
+          this._em.emit( "call.destroyed", this )
+        }, timeout )
+      }
+
+      newuac( options, callbacks ) {
+        mockinboundcall.newoutboundcallcount++
+        this._killcalls( options, callbacks, new mockagentcall( options.entity.uri, this, callbacks ) )
+      }
+    }
+
+    let qitems = [ {
+      "call": new mockinboundcall(),
+      "name": "fifoname",
+      "domain": "dummy.com",
+      "mode": "ringall",
+      "timeout": 1
+    }, {
+      "call": new mockinboundcall(),
+      "name": "fifoname",
+      "domain": "dummy.com",
+      "mode": "ringall",
+      "timeout": 1
+    } ]
+
+    /* now back to our inbound call */
+    let waitforfirst = mainfifo.queue( qitems[ 0 ] )
+    let waitforsecond = mainfifo.queue( qitems[ 1 ] )
+
+    expect( mainfifo.getdomain( "dummy.com" ).getfifo( "fifoname" ).size ).to.equal( 2 )
+
+    qitems[ 0 ].call.mockhangupafter( 300 )
+    
+    let reason = await waitforfirst
+    let reason2 = await waitforsecond
+
+    expect( mainfifo.getdomain( "dummy.com" ).getfifo( "fifoname" ).size ).to.equal( 0 )
+
+    expect( qitems[ 0 ].call.vars.fifo.epochs.leave - qitems[ 0 ].call.vars.fifo.epochs.enter ).to.be.below( 3 ) /* 1S */
+    expect( qitems[ 0 ].call.vars.fifo.state ).to.equal( "abandoned" )
+
+    expect( reason ).to.equal( "abandoned" )
+    expect( reason2 ).to.equal( "timeout" )
+
+    expect( qitems[ 0 ].call.mockfifoupdates ).to.equal( 1 )
+    expect( qitems[ 0 ].call.mockfifopositions[ 0 ] ).to.equal( 0 )
+
+    expect( qitems[ 1 ].call.mockfifoupdates ).to.equal( 2 )
+    expect( qitems[ 1 ].call.mockfifopositions[ 0 ] ).to.equal( 1 )
+    expect( qitems[ 1 ].call.mockfifopositions[ 1 ] ).to.equal( 0 )
+
+  } )
 } )
